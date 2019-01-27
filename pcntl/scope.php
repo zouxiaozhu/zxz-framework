d6acd8820aba5a0e874cd24274a946fbb485d230
张龙 <zhanglong@julanling.com>
2019-01-27 17:34:22 +0800

    test scope

---
+++ pcntl/scope.php
@@ -0,0 +1,36 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: zhanglong
+ * Date: 2019/1/27
+ * Time: 5:27 PM
+ */
+
+class scope
+{
+    public $a = 1;
+
+    public function test_scope($data, callable $callback)
+    {
+        echo $callback();
+    }
+}
+
+class test
+{
+    public $a = 3;
+
+    public function _test()
+    {
+        $scope = new scope();
+        $scope->test_scope('1', function () {
+            return $this->a;
+        });
+    }
+}
+
+$test = new test();
+
+$test->_test();
+
+// 结果居然是3  也就是$this 的作用域依然是test对象
